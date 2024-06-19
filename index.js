25765b05437aed3544023cd61433a7f66ddffae2
Kyle Greenidge <kylegreen149@gmail.com>
2024-06-19 15:41:24 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, partyType) {
+    const thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${partyType} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown() {
+    let i = 10
+    while (i > -1) {
+        console.log(i)
+        i--
+    }
+}
